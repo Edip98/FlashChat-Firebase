eb8b5aedca8aa2985e58cf4da56176ca0012703c
Edip98 <edipkhalilov@gmail.com>
2022-09-27 18:31:24 +0300

    Initial commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Эдип on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
